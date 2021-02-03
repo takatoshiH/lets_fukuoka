3d5ad578a32531e94af3c8c0220c3302f8c3e6b2
引尻貴俊 <hikijiri@hakobune.co.jp>
2021-02-03 18:57:49 +0900

    眠い

--- main.js
+++ main.js
@@ -62,22 +62,16 @@ if(!confirm('Do you like Fukuoka?')) {
 
 function startRoulette() {
     if(is_active) return;
-    closePopup();
-
     is_active = true;
-    document.getElementById('start_btn').removeAttribute('class');
-    document.getElementById('start_btn').setAttribute('class', 'start_btn_inactive');
-    document.getElementById('stop_btn').removeAttribute('class');
-    document.getElementById('stop_btn').setAttribute('class', 'stop_btn_active');
+
+    closePopup();
+    stopButtonActive();
 
     Array.from(document.getElementsByClassName('answer')).forEach(( answer ) =>  {
         answer.innerText = '';
     });
 
-    Array.from(document.getElementsByClassName('answer')).forEach((answer) => {
-        answer.classList.remove('answer_active');
-        answer.classList.add('answer_inactive');
-    });
+    changeStateIactive();
 
     intervalId = setInterval(() => {
         document.getElementById(municipality['id']).setAttribute('fill', 'white');
@@ -89,36 +83,14 @@ function startRoulette() {
 
 function stopRoulette() {
     if (!is_active) return;
-
-    document.getElementById('start_btn').removeAttribute('class');
-    document.getElementById('start_btn').setAttribute('class', 'start_btn_active');
-    document.getElementById('stop_btn').removeAttribute('class');
-    document.getElementById('stop_btn').setAttribute('class', 'stop_btn_inactive');
-
-    Array.from(document.getElementsByClassName('answer')).forEach((answer) => {
-        answer.classList.remove('answer_inactive');
-        answer.classList.add('answer_active');
-    });
-
-
-    // todo 答えが重複する問題の解決
     is_active = false;
+
+    stopButtonInctive();
+    changeStateActive();
+
     document.getElementById('start_btn').disabled = false;
-    let answers = [
-        municipality['name'],
-        municipalities[makeIncorrectIndex(index) ]['name'],
-        municipalities[makeIncorrectIndex(index) ]['name'],
-        municipalities[makeIncorrectIndex(index) ]['name'],
-    ];
 
-    // answers.sort(function() { Math.random() - .5; });
-    shuffle(answers);
-
-    // ランダムになっていない
-    document.getElementById('answer_1').innerText = answers[0];
-    document.getElementById('answer_2').innerText = answers[1];
-    document.getElementById('answer_3').innerText = answers[2];
-    document.getElementById('answer_4').innerText = answers[3];
+    makeAnswers();
 
     Array.from(document.getElementsByClassName('answer')).forEach((answer) => {
         answer.onclick = () => {
@@ -129,20 +101,17 @@ function stopRoulette() {
     clearTimeout(intervalId);
 }
 
-// 回答のPOPUP表示
+// 解答のPOPUP表示
 function createPopup(municipality_name) {
     document.getElementById('popup').style.display = 'block';
     document.getElementById('popup_title').innerText = municipality_name === municipality['name'] ? '正解' : `違います。`;
     document.getElementById('municipality_content').innerText = municipality_name !== municipality['name'] ? `正解は ${municipality['name']}です` : '';
     document.getElementById('municipality_url').href = municipality['url'];
 
-    Array.from(document.getElementsByClassName('answer')).forEach((answer) => {
-        answer.classList.remove('answer_active');
-        answer.classList.add('answer_inactive');
-    });
+    changeStateIactive();
 }
 
-// 回答のPOPUP停止
+// 解答のPOPUPを閉じる
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
     Array.from(document.getElementsByClassName('answer')).forEach((answer) => {
@@ -158,13 +127,47 @@ function makeIncorrectIndex(index) {
     }
 
     return Math.round(incorectIndex);
-
 }
 
-const shuffle = ([...array]) => {
-    for (let i = array.length - 1; i >= 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+function changeStateActive() {
+    Array.from(document.getElementsByClassName('answer')).forEach((answer) => {
+        answer.classList.remove('answer_active');
+        answer.classList.add('answer_inactive');
+    });
+}
+
+function changeStateIactive() {
+    Array.from(document.getElementsByClassName('answer')).forEach((answer) => {
+        answer.classList.remove('answer_active');
+        answer.classList.add('answer_inactive');
+    });
+}
+
+function stopButtonActive() {
+    document.getElementById('start_btn').removeAttribute('class');
+    document.getElementById('start_btn').setAttribute('class', 'start_btn_inactive');
+    document.getElementById('stop_btn').removeAttribute('class');
+    document.getElementById('stop_btn').setAttribute('class', 'stop_btn_active');
+}
+
+function stopButtonInctive() {
+    document.getElementById('start_btn').removeAttribute('class');
+    document.getElementById('start_btn').setAttribute('class', 'start_btn_active');
+    document.getElementById('stop_btn').removeAttribute('class');
+    document.getElementById('stop_btn').setAttribute('class', 'stop_btn_inactive');
+}
+
+function makeAnswers() {
+    let answers = [
+        municipality['name'],
+        municipalities[makeIncorrectIndex(index) ]['name'],
+        municipalities[makeIncorrectIndex(index) ]['name'],
+        municipalities[makeIncorrectIndex(index) ]['name'],
+    ];
+
+
+    document.getElementById('answer_1').innerText = answers[0];
+    document.getElementById('answer_2').innerText = answers[1];
+    document.getElementById('answer_3').innerText = answers[2];
+    document.getElementById('answer_4').innerText = answers[3];
 }
